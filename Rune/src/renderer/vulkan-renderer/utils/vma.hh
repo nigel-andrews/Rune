d8e2ff1211a071e1105358c22d5ba917c7c40cbb
Nigel Andrews <nigel.andrews@epita.fr>
2025-02-24 22:15:30 +0100

    feat: debug: add feature macros for VMA debugging

--- Rune/src/renderer/vulkan-renderer/utils/vma.hh
+++ Rune/src/renderer/vulkan-renderer/utils/vma.hh
@@ -4,5 +4,13 @@
 #pragma GCC diagnostic ignored "-Wmissing-field-initializers"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wunused-variable"
+
+#if defined(DEBUG) || !defined(NDEBUG)
+#    define VMA_DEBUG_MARGINS 16
+#    define VMA_DEBUG_DETECT_CORRUPTION 1
+#    define VMA_DEBUG_INITIALIZE_ALLOCATIONS 1
+#endif
+
 #include <vk_mem_alloc.h>
+
 #pragma GCC diagnostic pop
